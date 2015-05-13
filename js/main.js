a969697163a3bc3cbc916dc6ff97815abf6ce375
Sarah Louise Leong <slouise@live.com.my>
2015-05-13 11:28:00 -0700

    Added skeleton for skills and travels

--- js/main.js
+++ js/main.js
@@ -3,3 +3,22 @@ var app = angular.module('bodyapp',[
   	'ui.router',
 	'ui.bootstrap'
 	]);
+
+app.controller('MapsCtrl', ['$scope', 
+  function($scope) {
+  	$scope.greeting="testing ctrl";
+
+  	var initialize = function(){
+
+		var mapOptions = {
+		zoom: 2,
+		center: new google.maps.LatLng(0,30)
+		};
+
+		var map = new google.maps.Map(document.getElementById('map-canvas'),
+		  mapOptions);
+
+  	}
+
+  	initialize();
+}]);
