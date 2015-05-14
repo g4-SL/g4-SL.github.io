8ee08974003f28238dff4921cdc1420d02abe003
Sarah Louise Leong <slouise@live.com.my>
2015-05-14 00:21:40 -0700

    Added new skills

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # g4-SL.github.io
 Personal portfolio containing a list of projects and relevant information about the developer
+
+Used: Google Maps Api
+ngDialog
--- js/main.js
+++ js/main.js
@@ -1,24 +1,87 @@
 var app = angular.module('bodyapp',[
 	'ngAnimate', 
+	'ngDialog',
   	'ui.router',
 	'ui.bootstrap'
 	]);
 
-app.controller('MapsCtrl', ['$scope', 
-  function($scope) {
+app.controller('MapsCtrl', ['$scope', function($scope) {
   	$scope.greeting="testing ctrl";
 
   	var initialize = function(){
 
 		var mapOptions = {
-		zoom: 2,
-		center: new google.maps.LatLng(0,30)
+		zoom: 9,
+		center: new google.maps.LatLng(49.2569684, -123.1239135)
 		};
 
 		var map = new google.maps.Map(document.getElementById('map-canvas'),
 		  mapOptions);
 
-  	}
+  	};
+
+	var sketchProc = function(processing){
+		// Override draw function, by default it will be called 60 times per second
+		processing.draw = function() {
+		// determine center and max clock arm length
+		var centerX = processing.width / 2, centerY = processing.height / 2;
+		var maxArmLength = Math.min(centerX, centerY);
+
+		function drawArm(position, lengthScale, weight) {      
+		  processing.strokeWeight(weight);
+		  processing.line(centerX, centerY, 
+		    centerX + Math.sin(position * 2 * Math.PI) * lengthScale * maxArmLength,
+		    centerY - Math.cos(position * 2 * Math.PI) * lengthScale * maxArmLength);
+		}
+
+		// erase background
+		processing.background(224);
+
+		var now = new Date();
+
+		// Moving hours arm by small increments
+		var hoursPosition = (now.getHours() % 12 + now.getMinutes() / 60) / 12;
+		drawArm(hoursPosition, 0.5, 5);
+
+		// Moving minutes arm by small increments
+		var minutesPosition = (now.getMinutes() + now.getSeconds() / 60) / 60;
+		drawArm(minutesPosition, 0.80, 3);
+
+		// Moving hour arm by second increments
+		var secondsPosition = now.getSeconds() / 60;
+		drawArm(secondsPosition, 0.90, 1);
+		};
+	};
+
+/*	var canvas = document.getElementById("canvas1");
+	// attaching the sketchProc function to the canvas
+	var p = new Processing(canvas, sketchProc);
+	// p.exit(); to detach it*/
 
   	initialize();
 }]);
+
+app.controller('dialogCtrl', function($scope, ngDialog){
+	$scope.openPicDialog = function(){
+		ngDialog.open({
+			template: 'test.html',
+			controller: ['$scope', function($scope) {
+				$scope.myInterval = 3000;
+				$scope.slides = [
+				{
+				  image:'img/olympia/1.png'
+				},
+				{
+				  image: 'http://lorempixel.com/400/200/food'
+				},
+				{
+				  image: 'http://lorempixel.com/400/200/sports'
+				},
+				{
+				  image: 'http://lorempixel.com/400/200/people'
+				}
+				];
+			}]
+		});
+	}
+});
